570662d12da67565bd9745c5d180a99342e067fe
Michael Garee <m.garee@gmail.com>
2012-02-11 15:41:24 -0800

    Placeholder for game router.

---
+++ engine/client_manager.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Client management class for web server.
+
+"""
+from uuid import uuid1
+
+class ClientManager:
+    """
+
+    """
+    def __init__(self):
+        """"""
+
+
+    def add_to_group(self, client, group):
+        """Add client to group."""
+
+    def create_client(self, ident):
+        """Create client in internal map. Return ??"""
+
+    def create_group(self, name, ident):
+        """Create group."""
+
+    
+
+
+
+
+
+
+
+
+
+
+"""
+in from client: guid, data
+out to client: data
+
+server needs client/group values for traffic routing
+
+This can perform message filtering
+
+
+This:
+map containing `client guid`, `group id`
+
+
+maybe extend base class for cinch-specifics?
+
+"""
