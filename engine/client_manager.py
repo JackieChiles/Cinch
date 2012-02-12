aded1d8d247167654c6801cf5b1210f493e33b89
Michael Garee <m.garee@gmail.com>
2012-02-11 23:35:31 -0800

    Initial hack at client management. Feedback required.

--- engine/client_manager.py
+++ engine/client_manager.py
@@ -2,49 +2,172 @@
 """Client management class for web server.
 
 """
-from uuid import uuid1
+#from uuid import uuid4  # don't really need 32-byte ID
+import random
+
+# Constants for client id elements
+CLIENT_NAME = 0
+CLIENT_GROUP = 1
+CLIENT_PLAYER_NUM = 2
+
+MAX_GUID = 32767
 
 class ClientManager:
-    """
+    """Provide mapping functions for Clients with respect to the game engine.
 
+    self.clients is a dictionary of lists, keyed by client guid.
+    
+    Clients have the following associations (keys):
+    - name (str): user name for client
+    - group (int): group/game number
+    - playerNum (int): ID of Client within group
+    
     """
     def __init__(self):
         """"""
+        self.clients = dict()
+        self.groups = dict()
+        
+    def add_client(self, name=None):
+        """Create new client in internal map. Return client guid.
 
+        name (str): player/user name for client [optional]
+        
+        """
+        count = 0
+        while True:
+            guid = random.randint(0, MAX_GUID)
 
-    def add_to_group(self, client, group):
-        """Add client to group."""
+            # Verify guid unique in ClientManager
+            if guid not in self.clients:
+                break
 
-    def create_client(self, ident):
-        """Create client in internal map. Return ??"""
+            # Prevent infinite loops
+            count += count
+            if count == MAX_GUID:
+                raise RuntimeError(
+                    "Too many clients. Cannot create new client.")
+               
+        self.clients[guid] = [name, None, None] # Initialize client
 
-    def create_group(self, name, ident):
-        """Create group."""
+        return guid
 
-    
+    def add_client_to_group(self, client, group):
+        """Add client to group.
 
+        In current implementation, clients can belong to at most 1 group.
 
+        client (int): guid for client
+        group (int): id for group
+        
+        """
+        assert (client in self.clients.keys())
+        assert (group in self.groups.keys())
 
+        # Add group to client's info and reset playerNum
+        self.clients[client][CLIENT_GROUP] = group
+        self.clients[client][CLIENT_PLAYER_NUM] = None   # clear playerNum
 
+        # Add client to group's info
+        self.groups[group].append(client)
 
+    def add_group(self, ident):
+        """Create group.
 
+        ident (int): id for group
 
+        """
+        if ident in self.groups.keys():
+            raise Exception("Group {0} already exists.".format(ident))
+        
+        self.groups[ident] = None
 
+    def del_client(self, ident):
+        """Delete client and remove from its group (if applicable).
 
+        ident (int): guid for client
 
-"""
-in from client: guid, data
-out to client: data
+        """
+        try:
+            group = self.clients[ident][CLIENT_GROUP]
+            del self.clients[ident]
 
-server needs client/group values for traffic routing
+        except KeyError:
+            raise KeyError("Client {0} does not exist.".format(ident))
 
-This can perform message filtering
+        if groups is not None:
+            try:
+                self.groups[group].remove(ident)
+            except KeyError:
+                raise KeyError("Group {0} does not exist.".format(group))
 
+    def del_group(self, ident):
+        """Delete group and clear group assignments of associated clients.
 
-This:
-map containing `client guid`, `group id`
+        ident (int): id for group
 
+        """
+        try:
+            for client in self.groups[ident]:
+                client[CLIENT_GROUP] = None
 
-maybe extend base class for cinch-specifics?
+            del self.groups[ident]
+        except KeyError:
+            raise KeyError("Group {0} does not exist.".format(ident))
 
-"""
+    def get_clients_in_group(self, group):
+        """Return list of clients associated with group.
+
+        group (int): id for group
+
+        """
+        try:
+            return self.groups[group]
+        except KeyError:
+            raise KeyError("Group {0} does not exist.".format(group))
+
+    def get_group_by_client(self, client):
+        """Return group that contains client.
+
+        client (int): guid of client
+
+        """
+        try:
+            return self.clients[client][CLIENT_GROUP]
+        except KeyError:
+            raise KeyError("Client {0} does not exist.".format(client))
+
+    def get_player_num_by_client(self, client):
+        """Return playerNum for client.
+
+        client (int): guid of client
+
+        """
+        try:
+            return self.clients[client][CLIENT_PLAYER_NUM]
+        except KeyError:
+            raise KeyError("Client {0} does not exist.".format(client))            
+
+    def set_client_name(self, client, name):
+        """Set name for client.
+
+        client (int): guid for client
+        name (str): player name for client
+
+        """
+        assert (client in self.clients.keys())
+        assert isinstance(name, str)
+        
+        self.clients[client][CLIENT_NAME] = name
+
+    def set_client_player_num(self, client, playerNum):
+        """Set player number for client.
+
+        client (int): guid for client
+        playerNum (int): id for player within player's group
+
+        """
+        assert (client in self.clients.keys())
+        assert isinstance(playerNum, int)
+        
+        self.clients[client][CLIENT_PLAYER_NUM] = playerNum
