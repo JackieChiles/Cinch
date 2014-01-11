78753bb06dd88ac97599b7d68ed62222b4cebb7a
mrpoodlepants <jbgaynor@ieee.org>
2014-01-11 15:13:19 -0500

    Reverted scripts/server.config.js

--- scripts/server.config.js
+++ scripts/server.config.js
@@ -8,5 +8,3 @@ else {
     // Customize the following line to match the external server.
     var host_path = "http://www.example.com:65000/cinch";
 }
-
-var host_path = "http://localhost:8088/cinch";
