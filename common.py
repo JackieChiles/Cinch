de6a3c06f88c77c88b4d3f07d52588d0deae4969
PheonixKotori <m.garee@gmail.com>
2014-05-31 21:39:48 -0700

    Moves common.py to top level.

--- common.py
+++ common.py
@@ -3,6 +3,9 @@
 Non-module specific items useful to all components.
 """
 
+from db.dal import DAL, Field
+db = DAL('sqlite://cinch.sqlite',folder='db')
+
 
 def enum(**enums):
     """Create enum-type object. Create as: Numbers=enum(ONE=1, TWO=2)"""
