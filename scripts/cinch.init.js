95fc337cf0580452d1156d8b30f27eb48d2357b2
JackieChiles <dtgaynor@gmail.com>
2014-06-29 20:52:00 -0400

    Added handling of incompatible browsers. Closes #36.

--- scripts/cinch.init.js
+++ scripts/cinch.init.js
@@ -3,39 +3,44 @@
 CinchApp.viewModel = new CinchViewModel();
 
 $(function () {
-    var socket = CinchApp.socket;
+    if(Modernizr.canvas && Modernizr.canvastext && Modernizr.websockets) {
+        var socket = CinchApp.socket;
 
-    //Add warning when navigating away from game
-    window.onbeforeunload = CinchApp.viewModel.navigateAwayMessage;
+        //Add warning when navigating away from game
+        window.onbeforeunload = CinchApp.viewModel.navigateAwayMessage;
 
-    //Apply Knockout bindings
-    ko.applyBindings(CinchApp.viewModel);
+        //Apply Knockout bindings
+        ko.applyBindings(CinchApp.viewModel);
 
-    //Switch to the home view
-    CinchApp.viewModel.activeView(CinchApp.views.home);
+        //Switch to the home view
+        CinchApp.viewModel.activeView(CinchApp.views.home);
 
-    //Set up socket listeners
-    CinchApp.viewModel.setUpSocket();
-        
-    //Add a binding to the chat input to submit chats when enter is pressed
-    $('.chat-text').keypress(function(event) {
-        if ( event.which == 13 ) {
-           event.preventDefault();
-           $(this).nextAll('.chat-submit-button:first').click();
-        }
-    });
+        //Set up socket listeners
+        CinchApp.viewModel.setUpSocket();
+            
+        //Add a binding to the chat input to submit chats when enter is pressed
+        $('.chat-text').keypress(function(event) {
+            if ( event.which == 13 ) {
+               event.preventDefault();
+               $(this).nextAll('.chat-submit-button:first').click();
+            }
+        });
 
-    //Add a binding to the username input to goto Lobby when enter is pressed
-    $('#username-input').keypress(function(event) {
-        if ( event.which == 13 ) {
-           event.preventDefault();
-           $('#enter-lobby-btn').click();
-        }
-    });
+        //Add a binding to the username input to goto Lobby when enter is pressed
+        $('#username-input').keypress(function(event) {
+            if ( event.which == 13 ) {
+               event.preventDefault();
+               $('#enter-lobby-btn').click();
+            }
+        });
 
-    //Set the width and height of the play surface canvas
-    $('#play-surface').attr('width', CinchApp.playSurfaceWidth).attr('height', CinchApp.playSurfaceHeight);
+        //Set the width and height of the play surface canvas
+        $('#play-surface').attr('width', CinchApp.playSurfaceWidth).attr('height', CinchApp.playSurfaceHeight);
 
-    //Focus on username entry field
-    $('#username-input').focus();
+        //Focus on username entry field
+        $('#username-input').focus();
+    }
+    else {
+        $('#browser-warning').fadeIn();
+    }
 });
