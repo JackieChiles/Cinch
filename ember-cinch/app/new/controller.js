36103795e34657bb49b6b74fe457f43bee7c180f
JackieChiles <dtgaynor@gmail.com>
2016-11-28 21:02:03 -0500

    Added init.

--- ember-cinch/app/new/controller.js
+++ ember-cinch/app/new/controller.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  agents: null,
+
   startGame() {
+  },
+
+  init() {
+    this._super(...arguments);
+    this.set('agents', []);
   }
 });
