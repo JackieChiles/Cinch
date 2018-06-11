60764be08329c3522b0bdc21a23ec37e82697ca4
JackieChiles <dtgaynor@gmail.com>
2018-06-10 20:16:26 -0400

    Add method to retrieve events from DynamoDB.

--- server/database/events.js
+++ server/database/events.js
@@ -3,16 +3,16 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
   region: 'us-east-1',
-  endpoint: "https://dynamodb.us-east-1.amazonaws.com"
+  endpoint: 'https://dynamodb.us-east-1.amazonaws.com'
 });
 
 module.exports = {
   publish(gameEvent) {
-    if (!gameEvent || !gameEvent.type) {
-      console.log('Did not publish event to dynamoDB because the event object was missing or had no "type" property');
+    if (!gameEvent) {
+      console.log('Did not publish event to dynamoDB because the event object was missing');
     }
 
-    console.log("Publishing event to dynamoDB...");
+    console.log('Publishing event to dynamoDB...');
 
     const docClient = new AWS.DynamoDB.DocumentClient();
 
@@ -21,9 +21,30 @@ module.exports = {
       Item: Object.assign({ id: uuid(), timestamp: new Date().toISOString() }, gameEvent)
     }, function (err, data) {
       if (err) {
-        console.error("Unable to publish event to dynamoDB. Error JSON:", JSON.stringify(err, null, 2));
+        console.error('Unable to publish event to dynamoDB. Error JSON:', JSON.stringify(err, null, 2));
       } else {
-        console.log("Published event to dynamoDB:", JSON.stringify(data, null, 2));
+        console.log('Published event to dynamoDB: ', gameEvent.type, gameEvent.gameId);
+      }
+    });
+  },
+
+  getGameEvents(gameId) {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+
+    const params = {
+      TableName: 'game-events',
+      IndexName: 'gameId-timestamp-index',
+      KeyConditionExpression: 'gameId = :gameId',
+      ExpressionAttributeValues: {
+        ':gameId': gameId
+      }
+    };
+
+    docClient.query(params, function (err, data) {
+      if (err) {
+        console.error('Unable to retrieve events from dynamoDB. Error JSON:', JSON.stringify(err, null, 2));
+      } else {
+        console.log('Retrieved game events: ', gameId);
       }
     });
   }
